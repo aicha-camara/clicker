d0d4bad6aef4440b01eb6a96648fc1eaa5e5261d
camara_aicha <aicha.ca@outlook.fr>
2024-04-29 10:46:40 +0200

    ajout du fichier script qui lance le jeu

---
+++ script2.js
@@ -0,0 +1,6 @@
+const boutonJouer = document.getElementById('boutonJouer');
+
+boutonJouer.addEventListener('click', () => {
+  // Mettez ici le code pour démarrer le jeu, par exemple, redirigez vers la page principale du jeu :
+  window.location.href = "index.html"; // Remplacez "jeu.html" par le nom de votre fichier de jeu
+});
