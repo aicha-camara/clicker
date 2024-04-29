ea6fb79ce8635b3239af481606d881423df29956
camara_aicha <aicha.ca@outlook.fr>
2024-04-29 10:29:55 +0200

    ajout d'une fonction qui recupere tout les element des balises du fichier html

---
+++ script.js
@@ -0,0 +1,16 @@
+// Récupérer les éléments du DOM
+const elementCompteDollars = document.getElementById('compteDollars');
+const boutonClic = document.getElementById('boutonClic');
+const boutonAchatAutoClick5s = document.getElementById('boutonAchatAutoClick5s');
+const boutonAchatAutoClick2s = document.getElementById('boutonAchatAutoClick2s');
+const boutonReset = document.getElementById('boutonReset');
+const autoClickerImg = document.getElementById('autoClickerImg');
+const boutonBaisserTemps = document.getElementById('boutonBaisserTemps');
+const elementNombreAutoClick = document.getElementById('nombreAutoClick');
+const intervalleAutoClick = document.getElementById('intervalleAutoClick');
+const boutonAmeliorerClick = document.getElementById('boutonameliorerclick');
+const boutonMultiplicateurTemporaire = document.getElementById('boutonMultiplicateurTemporaire');
+const multiplicateurAutoClick = document.getElementById('multiplicateurAutoClick');
+const boutonAchatEauGelee = document.querySelector('#achat_objectif .ressource:nth-child(1) .icone-ressource');
+const boutonAchatRocheLunaire = document.querySelector('#achat_objectif .ressource:nth-child(2) .icone-ressource');
+const boutonAchatRegolithe = document.querySelector('#achat_objectif .ressource:nth-child(3) .icone-ressource');
